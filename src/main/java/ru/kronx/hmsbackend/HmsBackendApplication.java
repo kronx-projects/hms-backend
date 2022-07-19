a858d5885db783d95646bf0b4ae4d622ca3d4394
Kirill <pak.kiruha@yandex.ru>
2022-07-20 02:55:20 +0300

    initial commit

---
+++ src/main/java/ru/kronx/hmsbackend/HmsBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.kronx.hmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmsBackendApplication.class, args);
+    }
+
+}
