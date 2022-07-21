90fc4cd440c6492a1dfbca15d664da060bf42884
Kirill <pak.kiruha@yandex.ru>
2022-07-21 19:50:29 +0300

    создание структуры проекта #PMS-11

--- src/main/java/ru/kronx/hmsbackend/controller/TestController.java
+++ src/main/java/ru/kronx/hmsbackend/controller/TestController.java
@@ -1,4 +1,4 @@
-package ru.kronx.hmsbackend.controllers;
+package ru.kronx.hmsbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
